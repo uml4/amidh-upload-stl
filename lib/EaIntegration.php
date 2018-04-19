4320fed5b3fceb2afcb6b01d32de8e85d5f706ba
uml4it <thuantrinh89@gmail.com>
2018-04-19 16:00:37 +0700

    add class EaIntegration to integrate to EA service

---
+++ lib/EaIntegration.php
@@ -0,0 +1,123 @@
+<?php
+namespace Bookly\Lib;
+
+use Bookly\Lib;
+
+/**
+ * Class Scheduler
+ * @package BooklyRecurringAppointments\Lib
+ */
+class EaIntegration
+{
+    const REPEAT_DAILY    = 'daily';
+    const REPEAT_WEEKLY   = 'weekly';
+    const REPEAT_BIWEEKLY = 'biweekly';
+    const REPEAT_MONTHLY  = 'monthly';
+    const REPEAT_YEARLY   = 'yearly';//not implemented yet
+
+    private $ea_api_url;
+
+    private $ea_user_name;
+
+    private $ea_user_pass;
+
+   private $request_header;
+
+
+    /**
+     * Constructor.    
+     */
+    public function __construct(  )
+    {
+        // Set up UserBookingData.
+        $this->ea_api_url = esc_attr( get_option( 'ab_settings_ea_api_url' , '' ) );
+        $this->ea_user_name = esc_attr( get_option( 'ab_settings_ea_admin_user_name' , '' ) );
+        $this->ea_user_pass = esc_attr( get_option( 'ab_settings_ea_admin_password' , '' ) );
+            
+        $this->request_header = array(
+                                 'Authorization' => 'Basic ' . base64_encode( $this->ea_user_name . ':' . $this->ea_user_pass )
+                                    );
+        
+    }
+
+    
+    
+    public function getProviders($id = null ) {
+        
+        $url = $this->ea_api_url; 
+        if($id) {
+            $url = $url.'providers/'.$id;
+        } else {
+            $url = $url.'providers/';
+        }
+        
+        $request = new \WP_Http();
+        $response = $request->get( $url, $this->request_header );
+        
+        $response_code = $response['response']['code'];
+        
+        if($response_code == 200 ) {
+            return json_decode($response['body'],true);
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public function getServices($id = null ) {
+        
+        $url = $this->ea_api_url; 
+        if($id) {
+            $url = $url.'services/'.$id;
+        } else {
+            $url = $url.'services/';
+        }
+        
+       return (array) $this->proceedRequest($url);
+        
+    }
+
+    
+    /**
+     * 
+     * @param type $url
+     * @param type $req_type
+     * @param type $req_params
+     * @return \Bookly\Lib\HttpRespone
+     */
+    private function proceedRequest($url, $req_type = 'GET', $req_params = array() ) {
+        
+        $request = new \WP_Http();
+        $agr['headers'] = $this->request_header;
+        
+        
+        
+        if(strtoupper($req_type) == 'POST') {
+            if(count($req_params)) {
+                $agr['body'] = json_encode($req_params);
+            }
+            
+            $response = $request->post( $url, $agr );
+            
+        } else {
+            $response = $request->get( $url, $agr );
+        }
+        
+        $httpResponse = new HttpRespone();
+        $response_code = $response['response']['code'];
+        $httpResponse->code = $response_code;
+        //var_dump($response);
+        if($response_code == HttpRespone::CODE_404 || $response_code == HttpRespone::CODE_500
+                || $response_code == HttpRespone::CODE_401 
+                ) {
+            $httpResponse->error_message = $response['body'];
+            
+        } else {
+            $httpResponse->body = json_decode($response['body'],true);
+        }
+        
+        return $httpResponse;
+    } 
+    
+    
+}
---
+++ lib/HttpRespone.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Bookly\Lib;
+
+/**
+ * Description of ResponeRequest
+ *
+ * @author Owner
+ */
+class HttpRespone {
+    
+    const CODE_401 = 401;
+    const CODE_404 = 404;
+    const CODE_200 = 200;
+    const CODE_201 = 201;
+    const CODE_500 = 500;
+
+
+    public $code;
+    public $body = array();
+    public $error_message; 
+    
+    
+    
+}
